192502a975fea90189c97e9ed1f8a4adc089c7be
DavitMchedlishvili <davit.mchedlishvili.2396@gmail.com>
2022-12-11 05:04:27 +0400

    All done
    did my best

---
+++ burgermanu.js
@@ -0,0 +1,16 @@
+
+    const hamburger = document.querySelector(".hamburger");
+    const headerleft = document.querySelector(".headerleft");
+    
+    
+    hamburger.addEventListener("click", function(){
+        hamburger.classList.toggle ("is-active")
+        headerleft.classList.toggle("is-active")
+    });
+
+
+
+
+
+
+
